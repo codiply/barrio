f7def853d6df0c6393dbbd1e0d07da53fd1c4ca2
Panos <codiply@gmail.com>
2017-07-07 21:20:12 +0100

    Add helper for loading points from file

---
+++ src/main/scala/barrio/input/PointLoader.scala
@@ -0,0 +1,16 @@
+package com.codiply.barrio.input
+
+import scala.io.Source
+import com.codiply.barrio.nn.Point
+
+object PointLoader {
+  def fromFile(fileName: String): Iterable[Point] = {
+    Source.fromFile(fileName).getLines.toIterable
+      .map { _.split(",").map(_.trim) }
+      .map ( pieces => {
+        val id = pieces(0)
+        val coordinates = pieces.drop(1).map(_.toDouble).toList
+        Point(id, coordinates)
+      })
+  }
+}
