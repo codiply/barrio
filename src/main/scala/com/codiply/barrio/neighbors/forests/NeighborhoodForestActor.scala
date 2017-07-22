374441cac20d2f8fdafd15fe5841b970883257c4
Panos <codiply@gmail.com>
2017-07-22 11:02:05 +0100

    Sketch a neighborhood forest actor

---
+++ src/main/scala/com/codiply/barrio/neighbors/forests/NeighborhoodForestActor.scala
@@ -0,0 +1,28 @@
+package com.codiply.barrio.neighbors.forests
+
+import scala.concurrent.duration._
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+import com.codiply.barrio.neighbors.NeighborhoodPatchActorProtocol._
+import com.codiply.barrio.neighbors.NeighborAggregatorActor
+import com.codiply.barrio.neighbors.Point
+import com.codiply.barrio.neighbors.Point._
+
+class NeighborhoodForestActor(
+    points: List[Point],
+    distance: DistanceMetric,
+    nTrees: Int,
+    aggregatorTimeout: FiniteDuration) extends Actor {
+  
+  val trees = (1 to nTrees).map(_ => 
+    context.actorOf(NeighborhoodTreeActor.props(points, distance))).toList
+  
+  def receive: Receive = {
+    case request @ GetNeighborsRequest(coordinates, k) => {
+      val originalSender = sender
+      val aggregator = context.actorOf(NeighborAggregatorActor.props(
+          coordinates, k, distance, originalSender, nTrees, aggregatorTimeout))
+      trees.foreach(_.tell(request, aggregator))
+    }
+  }
+}
