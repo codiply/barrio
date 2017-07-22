d65a3ee2f2f22cb29089c64a63ded2aa252f6fd0
Panos <codiply@gmail.com>
2017-07-22 09:03:48 +0100

    Make the aggregator actor generic

---
+++ src/main/scala/com/codiply/barrio/generics/AggregatorActor.scala
@@ -0,0 +1,39 @@
+package com.codiply.barrio.generics
+
+import scala.concurrent.duration._
+import scala.reflect.ClassTag
+
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+import akka.actor.ActorRef
+
+object AggregatorActorProtocol {
+  object DoSendAggregate
+}
+
+class AggregatorActor[TAggregate, TResponse:  ClassTag](
+    responseRecipient: ActorRef,
+    initialValue: TAggregate,
+    folder: (TAggregate, TResponse) => TAggregate,
+    mapper: TAggregate => TResponse,
+    expectedNumberOfResponses: Int,
+    timeout: FiniteDuration) extends Actor {
+  import AggregatorActorProtocol._
+  import context.dispatcher
+  
+  var aggregate: TAggregate = initialValue
+  var outstandingResponses = expectedNumberOfResponses
+ 
+  context.system.scheduler.scheduleOnce(timeout, self, DoSendAggregate)
+  
+  def receive: Receive = {
+    case response: TResponse =>
+      this.aggregate = folder(this.aggregate, response)
+      this.outstandingResponses -= 1
+      if (this.outstandingResponses <= 0) self ! DoSendAggregate
+    case DoSendAggregate =>
+      val response = mapper(this.aggregate)
+      responseRecipient ! response
+      context.stop(self)
+  }
+}
--- src/main/scala/com/codiply/barrio/neighbors/NeihgborAggregatorActor.scala
+++ src/main/scala/com/codiply/barrio/neighbors/NeihgborAggregatorActor.scala
@@ -2,11 +2,10 @@ package com.codiply.barrio.neighbors
 
 import scala.concurrent.duration._
 
-import akka.actor.Actor
-import akka.actor.Actor.Receive
 import akka.actor.ActorRef
 import akka.actor.Props
-import akka.actor.PoisonPill
+import com.codiply.barrio.generics.AggregatorActor
+import NeighborhoodPatchActorProtocol._
 import Point._
 
 object NeighborAggregatorActor {
@@ -14,40 +13,13 @@ object NeighborAggregatorActor {
       coordinates: Coordinates,
       kNeighbors: Int,
       distance: DistanceMetric,
-      replyTo: ActorRef,
+      responseRecepient: ActorRef,
       expectedNumberOfResponses: Int,
-      timeout: FiniteDuration) = Props(new NeighborAggregatorActor(
-          coordinates, kNeighbors, distance, replyTo, expectedNumberOfResponses, timeout))
-}
-
-object NeighborAggregatorActorProtocol {
-  object DoSendAggregate
-}
-
-class NeighborAggregatorActor(
-    coordinates: Coordinates,
-    kNeighbors: Int,
-    distance: DistanceMetric,
-    originalQuerySender: ActorRef,
-    expectedNumberOfResponses: Int,
-    timeout: FiniteDuration) extends Actor {
-  import NeighborhoodPatchActorProtocol._
-  import NeighborAggregatorActorProtocol._
-  import context.dispatcher
-  
-  var nearestNeighbors: List[Point] = Nil
-  var outstandingResponses = expectedNumberOfResponses
- 
-  context.system.scheduler.scheduleOnce(timeout, self, DoSendAggregate)
-  
-  def receive: Receive = {
-    case GetNeighborsResponse(neighbors) =>
-      this.nearestNeighbors = (nearestNeighbors ++ neighbors).sortBy(p => 
-        distance(coordinates, p.coordinates)).take(kNeighbors)
-      this.outstandingResponses -= 1
-      if (this.outstandingResponses <= 0) self ! DoSendAggregate
-    case DoSendAggregate => 
-      originalQuerySender ! GetNeighborsResponse(nearestNeighbors)
-      context.stop(self)
-  }
+      timeout: FiniteDuration) = {
+        val initialValue = Nil
+        val folder = (neighbors: List[Point], response: GetNeighborsResponse) => 
+          (neighbors ++ response.neighbors).sortBy(p => distance(coordinates, p.coordinates)).take(kNeighbors)
+        val mapper = (neighbors: List[Point]) => GetNeighborsResponse(neighbors)
+        Props(new AggregatorActor(responseRecepient, initialValue, folder, mapper, expectedNumberOfResponses, timeout))
+      }
 }
