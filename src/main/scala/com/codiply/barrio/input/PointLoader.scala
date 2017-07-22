671e7bc0b7708619adef8ecad993eeb99fea87de
Panos <codiply@gmail.com>
2017-07-23 00:45:41 +0100

    Skip empty lines when loading data

--- src/main/scala/com/codiply/barrio/input/PointLoader.scala
+++ src/main/scala/com/codiply/barrio/input/PointLoader.scala
@@ -6,6 +6,7 @@ import com.codiply.barrio.neighbors.Point
 object PointLoader {
   def fromFile(fileName: String): Iterable[Point] = {
     Source.fromFile(fileName).getLines.toIterable
+      .filter { !_.isEmpty() }
       .map { _.split(",").map(_.trim) }
       .map ( pieces => {
         val id = pieces(0)
