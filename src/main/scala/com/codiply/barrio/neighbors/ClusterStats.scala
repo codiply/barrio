2572c20f208926626a44ab3e4a9cf6955474a5fb
Panos <codiply@gmail.com>
2017-08-03 18:03:10 +0100

    Calculate tree stats using the stats container

--- src/main/scala/com/codiply/barrio/neighbors/ClusterStats.scala
+++ src/main/scala/com/codiply/barrio/neighbors/ClusterStats.scala
@@ -1,20 +1,46 @@
 package com.codiply.barrio.neighbors
 
+import com.codiply.barrio.helpers.LongQuantityStats
+import com.codiply.barrio.helpers.LongQuantityStatsContainer
+
 final case class MemoryStats(
   freeMemoryMB: Double,
   totalMemoryMB: Double,
   maxMemoryMB: Double,
   usedMemoryMB: Double)
 
+final case class LeafStats(
+    depth: Int,
+    pointCount: Int)
+
+object TreeStatsContainer {
+  val empty: TreeStatsContainer =
+    TreeStatsContainer(
+        pointsPerLeaf = LongQuantityStatsContainer.empty,
+        depth = LongQuantityStatsContainer.empty)
+}
+
+final case class TreeStatsContainer(
+    pointsPerLeaf: LongQuantityStatsContainer,
+    depth: LongQuantityStatsContainer) {
+  def add(stats: LeafStats): TreeStatsContainer =
+    TreeStatsContainer(
+        pointsPerLeaf = pointsPerLeaf.add(stats.pointCount),
+        depth = depth.add(stats.depth))
+
+  def toStats(): TreeStats =
+    TreeStats(
+        leafs = pointsPerLeaf.count,
+        points = pointsPerLeaf.sum,
+        pointsPerLeaf = pointsPerLeaf.toStats,
+        depth = depth.toStats)
+}
+
 final case class TreeStats(
-  leafs: Int,
-  points: Int,
-  minLeafPoints: Int,
-  meanLeafPoints: Double,
-  maxLeafPoints: Int,
-  minDepth: Int,
-  meanDepth: Double,
-  maxDepth: Int)
+  leafs: BigInt,
+  points: BigInt,
+  pointsPerLeaf: LongQuantityStats,
+  depth: LongQuantityStats)
 
 final case class NodeStats(
   memory: MemoryStats,
