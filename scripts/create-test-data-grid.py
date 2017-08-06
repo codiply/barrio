585ae036d4dee41a689462689fd49cc6e7c1a830
Panos <codiply@gmail.com>
2017-08-06 10:15:47 +0100

    Create test grid data for multiple dimensions

--- scripts/create-test-data-grid.py
+++ scripts/create-test-data-grid.py
@@ -2,30 +2,38 @@ import os
 from itertools import product
 from random import random, randint, seed
 
+# Configuration
+min_dimensions = 4
+max_dimensions = 7
 n_files = 4
-n_dimensions = 7
 min_value = -3
 max_value = 3
 step = 1
+noise_factor = 0.2
 
 seed(123)
 
-data_folder = "./data/grid/"
+data_folder = "./data/"
 
-if(not os.path.exists(data_folder)):
-    os.mkdir(data_folder)
+def createTestDataWithDimensions(n_dimensions):
+    folder = os.path.join(data_folder, "grid-" + str(d) + "-d")
+    if(not os.path.exists(folder)):
+        os.mkdir(folder)
 
-values = [list(range(min_value, max_value + step, step)) for d in range(n_dimensions)]
+    files = [open(os.path.join(folder, "input-" + str(i) + ".txt"), "w") for i in range(1, n_files + 1)]
 
-files = [open(os.path.join(data_folder, "input-" + str(i) + ".txt"), "w") for i in range(1, n_files + 1)]
+    coordinate_values = [list(range(min_value, max_value + step, step)) for d in range(n_dimensions)]
 
-for i, coordinates in enumerate(product(*values)):
-    coordinates_with_noise = [str(x + 0.2 * step * (random() - 0.5)) for x in coordinates]
-    file = files[randint(0, n_files - 1)]
+    for i, coordinates in enumerate(product(*coordinate_values)):
+        coordinates_with_noise = [str(x + noise_factor * step * (random() - 0.5)) for x in coordinates]
+        f = files[randint(0, n_files - 1)]
     
-    file.write("point-" + str(i) + ",")
-    file.write(",".join(coordinates_with_noise))
-    file.write("\n")
-    
-for f in files:
-    f.close()
+        f.write("point-" + str(i) + ",")
+        f.write(",".join(coordinates_with_noise))
+        f.write("\n")
+
+    for f in files:
+        f.close()
+        
+for d in range(min_dimensions, max_dimensions + 1):
+    createTestDataWithDimensions(d)
