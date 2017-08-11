bebdf5449b9fd24ee794ab8790d827a23c210f38
Panos <codiply@gmail.com>
2017-08-11 08:59:37 +0100

    Allow for different separators for id and coordinates

--- src/main/scala/com/codiply/barrio/helpers/PointLoader.scala
+++ src/main/scala/com/codiply/barrio/helpers/PointLoader.scala
@@ -3,26 +3,56 @@ package com.codiply.barrio.helpers
 import scala.io.Source
 
 import com.codiply.barrio.geometry.Point
+import com.codiply.barrio.geometry.Point.Coordinates
 
 object PointLoader {
-  def fromCsvFile(fileName: String, dimensions: Int, separator: String): Iterable[Point] = {
-    Source.fromFile(fileName)("UTF-8").getLines.toIterable
-      .filter { !_.isEmpty() }
-      .map { _.split(separator).map(_.trim) }
-      .flatMap( pieces => {
-        if (pieces.length == dimensions + 1) {
-          try {
-            val id = pieces(0)
-            val location = pieces.drop(1).map(_.toDouble).toList
-            Some(Point(id, location))
-          } catch {
-            case e: Exception =>
-              // TODO: log error
-              None
-          }
-        } else {
-          None
+  def fromCsvFile(
+      fileName: String,
+      dimensions: Int,
+      idSeparator: String,
+      coordinateSeparator: String): Seq[Point] = {
+    fromCsvLines(Source.fromFile(fileName)("UTF-8").getLines.toSeq,
+        dimensions, idSeparator = idSeparator, coordinateSeparator = coordinateSeparator)
+  }
+
+  def fromCsvLines(
+      lines: Seq[String],
+      dimensions: Int,
+      idSeparator: String,
+      coordinateSeparator: String): Seq[Point] = {
+    val parseCoordinates = createCoordinatesParser(dimensions, coordinateSeparator)
+    val parseCsvLine = createCsvLineParser(idSeparator, parseCoordinates)
+    lines.filter { !_.isEmpty() }.flatMap(parseCsvLine(_))
+  }
+
+  def createCsvLineParser(
+      idSeparator: String,
+      coordinatesParser: String => Option[Coordinates]): String => Option[Point] = {
+    (line: String) => {
+      val pieces = line.split(idSeparator).map(_.trim)
+      if (pieces.length == 2) {
+        val id = pieces(0)
+        coordinatesParser(pieces(1)).map { Point(id, _) }
+      } else {
+        None
+      }
+    }
+  }
+
+  def createCoordinatesParser(dimensions: Int, separator: String): String => Option[Coordinates] = {
+    (input: String) => {
+      val pieces = input.split(separator)
+      if (pieces.size == dimensions) {
+        try {
+          Some(pieces.map(_.toDouble).toList)
+        } catch {
+          case e: Exception =>
+            // TODO: log error
+            None
         }
-      })
+      } else {
+        None
+      }
+    }
   }
 }
