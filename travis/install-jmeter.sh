9bdd5763bffca02646106977f7a63fdc6906549f
Panos <codiply@gmail.com>
2017-09-24 16:33:58 +0100

    Upgrade JMeter to 3.3

--- travis/install-jmeter.sh
+++ travis/install-jmeter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 directory="$1"
-version=3.2
+version=3.3
 
 wget http://www.apache.org/dist//jmeter/binaries/apache-jmeter-${version}.tgz -O /tmp/apache-jmeter-${version}.tgz
 tar -xf /tmp/apache-jmeter-${version}.tgz
